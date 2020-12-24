199eb01b6b80a8c732b47fd079c55ecd4cc7b8e9
Johnson He <hejohns@umich.edu>
2020-12-24 08:20:44 +0000

    You get the point
    I've kind of lost interest in getting LH into xargs.
    You get the point.
    (Never did get around to just making a terminate-on-EOF
    recursive loop for xargs... whatever)

--- xargs2/Main.hs
+++ xargs2/Main.hs
@@ -11,19 +11,27 @@ import System.Process
 import System.IO
 import System.Exit
 
-{-
-nonEmptyString :: String -> Maybe NonEmptyString
-nonEmptyString str
-    | length str > 0 = Just $ NonEmptyString str
-    | otherwise = Nothing
+{-@ type NonEmptyString = NonEmptyList Char @-}
+{-@ type NonEmptyList a = {v : [a] | len v > 0} @-}
+
+{-@ nonEmptyString :: String -> Maybe NonEmptyString @-}
+nonEmptyString :: String -> Maybe String
+nonEmptyString "" = Nothing
+nonEmptyString str@(hd:tl) = Just str
+
+{-@ nonEmptyList :: [a] -> Maybe (NonEmptyList a) @-}
+nonEmptyList :: [a] -> Maybe [a]
+nonEmptyList [] = Nothing
+nonEmptyList l@(hd:tl) = Just l
 
 {- Data.Text replace, just wanted to write it myself -}
-substitute :: String -> NonEmptyString -> String -> String
+{-@ substitute :: String -> NonEmptyString -> String -> String @-}
+substitute :: String -> String -> String -> String
 substitute "" _ _ = ""
-substitute str@(hd:tl) match@(NonEmptyString match') r =
-    case length match' <= length str of
-        True -> case isPrefixOf match' str of
-            True -> r ++ substitute (drop (length match') str) match r
+substitute str@(hd:tl) match r =
+    case length match <= length str of
+        True -> case isPrefixOf match str of
+            True -> r ++ substitute (drop (length match) str) match r
             False -> hd:substitute tl match r
         False -> str
 
@@ -33,47 +41,23 @@ myFold f a@(hd:tl) = f hd >> myFold f tl
 main :: IO ()
 main = do
     argv <- getArgs
-    --if length argv
-    let maybeDelim = stripPrefix "-I" (head argv)
-    case (2 <= length argv, maybeDelim) of
-        (True, Just delim) ->
-            case nonEmptyString delim of
-                Just delim ->
-                    let a = substitute (concat (intersperse " " (tail argv))) delim in
-                    let b = repeat (isEOF >>= (\x -> if x then exitWith ExitSuccess else getLine)) in
-                    let c = myFold (>>= callCommand . a) b in
-                    --let c = traverse_ (>>= callCommand . a) b in
-                    --putStrLn $ substitute "Hello, Haskell!" (NonEmptyString "Hell") "G"
-                    --do
-                    --callCommand (a "ABC")
-                    c
-                _ -> help
-        (_, _) -> help
+    case nonEmptyList argv of
+        Nothing -> help
+        Just argv ->
+            let maybeDelim = stripPrefix "-I" (head argv) in
+            case (2 <= length argv, maybeDelim) of
+                (True, Just delim) ->
+                    case nonEmptyString delim of
+                        Just delim ->
+                            let a = substitute (concat (intersperse " " (tail argv))) delim in
+                            let b = repeat (isEOF >>= (\x -> if x then exitWith ExitSuccess else getLine)) in
+                            let c = myFold (>>= callCommand . a) b in
+                            --let c = traverse_ (>>= callCommand . a) b in
+                            --putStrLn $ substitute "Hello, Haskell!" (NonEmptyString "Hell") "G"
+                            --do
+                            --callCommand (a "ABC")
+                            c
+                        _ -> help
+                (_, _) -> help
     where
     help = putStrLn "Usage: xargs -I{} string"
--}
-
-{-@ type NonEmptyString = NonEmptyList Char @-}
-{-@ type NonEmptyList a = {v : [a] | Data.List.length v > 0} @-}
-
-{-@ measure Data.List.length :: [a] -> {v : Int | v >= 0} @-}
-
-{-@ impossible :: {v : String | false} -> a @-}
-impossible msg = error msg
-
-{-@ substitute :: String -> NonEmptyString -> String -> String @-}
-substitute :: String -> String -> String -> String
-substitute str match r = str
-
-{-@ nonEmptyString :: String -> Maybe NonEmptyString @-}
-nonEmptyString :: String -> Maybe String
-nonEmptyString "" = Nothing
-nonEmptyString str@(hd:tl) = Just (hd:tl)
-
-{-@ nonEmptyList :: [a] -> Maybe (NonEmptyList a) @-}
-nonEmptyList :: [a] -> Maybe [a]
-nonEmptyList [] = Nothing
-nonEmptyList l@(hd:tl) = Just (hd:tl)
-
-main :: IO()
-main = undefined
