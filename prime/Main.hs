d66c737a0f8b6f997af3973e2d2bd6e9f15c1d59
Johnson He <hejohns@umich.edu>
2020-12-31 01:32:00 +0000

    super broken but whatever, you get the point

--- prime/Main.hs
+++ prime/Main.hs
@@ -1,6 +1,7 @@
-{-# LANGUAGE RankNTypes, ScopedTypeVariables #-}
+{-# LANGUAGE CPP, RankNTypes, ScopedTypeVariables #-}
 module Main (main) where
 
+#if 0
 {-@ reflect noZeros @-}
 {-@ noZeros :: (Eq a, Num a) => [a] -> Bool @-}
 noZeros :: (Eq a, Num a) => [a] -> Bool
@@ -18,4 +19,23 @@ main :: IO ()
 main = putStrLn $ show $ prime p 0 1000
     where
 {-@ assume p :: {v : [Integer] | noZeros v} @-}
+    p = [2..10000]
+#endif
+
+{-@ ignore take' @-}
+take' :: (Eq a, Num a) => a -> [b] -> [b]
+take' 0 _ = []
+take' _ [] = []
+take' n l@(hd:tl) = hd:(take' (n - 1) tl)
+
+{-@ ignore prime @-}
+prime :: (Integral a, Eq b, Num b) => [a] -> b -> b -> [a]
+prime [] s u = []
+prime l@(h:t) s u
+    | s == u = take' s l
+    | otherwise = h:(prime (filter (\x -> (x `mod` h) /= 0) t) (s + 1) u)
+
+main :: IO ()
+main = putStrLn $ show $ prime p 0 10
+    where
     p = [2..10000]
