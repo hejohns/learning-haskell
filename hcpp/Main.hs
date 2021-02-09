77b2b3f380e4d75772fc0096c357dd5430dafa14
Johnson He <hejohns@umich.edu>
2021-02-09 02:07:01 +0000

    wow, it's been a while

--- hcpp/Main.hs
+++ hcpp/Main.hs
@@ -1,4 +1,38 @@
-module Main where
+{-# LANGUAGE GADTs, ScopedTypeVariables #-}
+{-@ LIQUID "--reflection" @-}
+module Main (main) where
+import System.Environment
+import System.Console.GetOpt
+import qualified Data.Map.Lazy as Map
 
+data ConsoleOptions where
+    Help :: ConsoleOptions
+    Version :: ConsoleOptions
+    deriving (Eq, Show)
+
+{-@ ignore main @-}
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = do
+    argv <- getArgs
+    let (opts :: [ConsoleOptions], nonOpts :: [String], errors :: [String]) = (getOpt__ argv)
+    let processOpts__ = opts
+    -- yes, I know this fails
+    case head opts of
+        Help -> putStrLn helpMesg__
+        Version -> putStrLn versionMesg__
+    if length nonOpts == 0
+    then
+        -- stdin
+        putStrLn "3"
+    else
+        putStrLn $ show $ head nonOpts
+    where
+    getOpt__ = getOpt
+        Permute
+        [
+            (Option ['h'] ["help"] (NoArg Help) helpMesg__),
+            (Option ['v'] ["version"] (NoArg Version) versionMesg__)
+        ]
+    helpMesg__ = "Usage: hcpp [-h] [-v]"
+    versionMesg__ = "hcpp 0.0\nWritten by Some Dummy"
+    --processOpts__ Help = a
